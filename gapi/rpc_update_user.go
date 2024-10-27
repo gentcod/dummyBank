0fbf6bd994b53056e83767653b78bda8adfb84bb
Oyefule <drelanorgent@gmail.com>
2024-10-27 21:46:29 +0100

    Implemented optional parameters for Updating user data

---
+++ gapi/rpc_update_user.go
@@ -0,0 +1,90 @@
+package gapi
+
+import (
+	"context"
+	"database/sql"
+	"time"
+
+	"github.com/gentcod/DummyBank/extensions"
+	db "github.com/gentcod/DummyBank/internal/database"
+	"github.com/gentcod/DummyBank/pb"
+	"github.com/gentcod/DummyBank/util"
+	"github.com/google/uuid"
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (server *Server) UpdateUser(ctx context.Context, req *pb.UpdateUserRequest) (*pb.UpdateUserResponse, error) {
+	violations := validateUpdateUserRequest(req)
+	if violations != nil {
+		return nil, valiateParameters(violations)
+	}
+
+	arg := db.UpdateUserParams{
+		ID: uuid.MustParse(req.GetId()),
+		FullName: sql.NullString{
+			String: req.GetFullName(),
+			Valid:  req.FullName != nil,
+		},
+		Email: sql.NullString{
+			String: req.GetEmail(),
+			Valid:  req.Email != nil,
+		},
+	}
+
+	if req.Password != nil {
+		hashedPassword, err := util.HashPassword(req.GetPassword())
+		if err != nil {
+			return nil, status.Errorf(codes.Internal, "failed to hash password: %v", err)
+		}
+
+		arg.HarshedPassword = sql.NullString{
+			String: hashedPassword,
+			Valid:  true,
+		}
+		arg.PasswordChangedAt = sql.NullTime{
+			Time:  time.Now().UTC(),
+			Valid: true,
+		}
+	}
+
+	user, err := server.store.UpdateUser(ctx, arg)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, status.Errorf(codes.NotFound, "user not found")
+		}
+		return nil, status.Errorf(codes.Internal, "failed to update user: %v", err)
+	}
+
+	rsp := &pb.UpdateUserResponse{
+		User: convertUser(user),
+	}
+	return rsp, nil
+}
+
+func validateUpdateUserRequest(req *pb.UpdateUserRequest) (violations []*errdetails.BadRequest_FieldViolation) {
+	if err := extensions.ValidateID(req.GetId()); err != nil {
+		violations = append(violations, fieldViolation("user_id", err))
+	}
+
+	if req.FullName != nil {
+		if err := extensions.ValidateFullname(req.GetFullName()); err != nil {
+			violations = append(violations, fieldViolation("full_name", err))
+		}
+	}
+
+	if req.Email != nil {
+		if err := extensions.ValidateEmail(req.GetEmail()); err != nil {
+			violations = append(violations, fieldViolation("email", err))
+		}
+	}
+
+	if req.Password != nil {
+		if err := extensions.ValidatePassword(req.GetPassword()); err != nil {
+			violations = append(violations, fieldViolation("password", err))
+		}
+	}
+
+	return
+}
